12c1d5a9707144005bcd89f0f0db5e79cbcbc41f
Gian Maria Ricci <alkampfer@nablasoft.com>
2022-10-28 18:56:05 +0200

    Added link retrieval

--- src/AzureDevopsExportQuickAndDirty/Program.cs
+++ src/AzureDevopsExportQuickAndDirty/Program.cs
@@ -60,11 +60,6 @@ namespace AzureDevopsExportQuickAndDirty
             var queryResult = conn.WorkItemStore.Query(query);
             var allWorkItems = queryResult.OfType<WorkItem>().ToList();
 
-            foreach (var item in allWorkItems)
-            {
-                Console.WriteLine(item.Fields["System.State"]);
-            }
-
             Log.Information("Loaded In memory {count} work items", allWorkItems.Count);
 
             //now we need to export all data in excel file.
@@ -75,8 +70,11 @@ namespace AzureDevopsExportQuickAndDirty
             ws.Cells["D1"].Value = "CreationDate";
             ws.Cells["E1"].Value = "CreatedBy";
             ws.Cells["F1"].Value = "AssignedTo";
+            ws.Cells["G1"].Value = "RelatedWorkItems";
+            ws.Cells["H1"].Value = "Code";
+            ws.Cells["I1"].Value = "PullRequest";
             Int32 row = 2;
-            foreach (WorkItem workItem in allWorkItems)
+            foreach (WorkItem workItem in allWorkItems.Take(1000))
             {
                 Log.Debug("Loaded work item {id}.", workItem.Id);
 
@@ -86,6 +84,10 @@ namespace AzureDevopsExportQuickAndDirty
                 ws.Cells[$"D{row}"].Value = workItem.CreatedDate.ToString("yyyy/MM/dd");
                 ws.Cells[$"E{row}"].Value = workItem.CreatedBy;
                 ws.Cells[$"F{row}"].Value = workItem.Fields["system.assignedTo"].Value;
+
+                ws.Cells[$"G{row}"].Value = workItem.Links.OfType<RelatedLink>().Count();
+                ws.Cells[$"H{row}"].Value = workItem.Links.OfType<ExternalLink>().Where(el => el.ArtifactLinkType.Name.Contains("Commit")).Count();
+                ws.Cells[$"I{row}"].Value = workItem.Links.OfType<ExternalLink>().Where(el => el.ArtifactLinkType.Name.Contains("Pull Request")).Count();
 
                 row++;
             }
